b64171cae42d566625a38b1f8a89acc2e3dea9b1
yakovevsasha1998-crypto <yakovevsasha1998@gmail.com>
2026-04-09 18:05:18 +0300

    Work

---
+++ cats_project/main.py
@@ -0,0 +1,17 @@
+import requests
+
+url = 'https://catfact.ninja/fact'
+
+try:
+    req = requests.get(url)
+    if req.status_code == 200:
+        info_json = req.json()
+        fact = info_json['fact']
+        
+        with open('fact.txt','a',encoding='utf-8') as file:
+            file.write(fact + '\n')
+        print('Успешно')
+    else:
+        print('Ошибка при получении факта')
+except Exception:
+    print('Не удалось получить fact')
