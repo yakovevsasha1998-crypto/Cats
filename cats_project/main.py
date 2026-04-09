f2824ef18bd5d14382099f4bea77539af08ca8a6
yakovevsasha1998-crypto <yakovevsasha1998@gmail.com>
2026-04-09 23:08:30 +0300

    Добавил немного комментариев и описал свой проект в README.md

--- cats_project/main.py
+++ cats_project/main.py
@@ -7,10 +7,10 @@ try:
     if req.status_code == 200:
         info_json = req.json()
         fact = info_json['fact']
-        
+        #открываем  файл после успешного взятия факта
         with open('fact.txt','a',encoding='utf-8') as file:
             file.write(fact + '\n')
-        print('Успешно')
+        print('Успешно') # успешная запись в файл
     else:
         print('Ошибка при получении факта')
 except Exception:
